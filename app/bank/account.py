c998a66826b56b57389b85da642dbd82de8efcde
Tomasz <justupgrade@o2.pl>
2015-12-14 20:29:57 +0100

    TDD

---
+++ app/bank/account.py
@@ -0,0 +1,5 @@
+class Account(object):
+
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
---
+++ test/bank/account_test.py
@@ -0,0 +1,14 @@
+import unittest
+from app.bank.account import Account
+
+
+class AccountTest(unittest.TestCase):
+
+    def test_create_valid_account(self):
+        account = Account("001", 50)
+        self.assertEqual(account.account_number, "001")
+        self.assertEqual(account.balance, 50)
+
+    def test_create_account(self):
+        account = Account('001', 0)
+
